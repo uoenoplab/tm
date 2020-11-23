cbf07cbbfab8cdb0968f091d1a2b1e14d6528366
Michio Honda <michio.honda@ed.ac.uk>
2020-11-23 21:21:03 +0000

    minor fix and copyright

--- tm.py
+++ tm.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3.8
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2020 Michio Honda.  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+#Falsemodification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the project nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 from argparse import ArgumentParser
 import sys
 import pyipmi
@@ -374,12 +400,11 @@ class Tm(object):
                                 print('{}: MAC and IP address found in '
                                       '/etc/dnsmasq.conf'.format(node['node']))
                                 break
+                            m = '{}: inventory {} registered {}'
                             if node['mac'] != mac:
-                                print('{}: inventory {} registered {}'.format(
-                                    node['mac'], mac))
+                                print(m.format(name, node['mac'], mac))
                             if node['ip'] != ip:
-                                print('{}: inventory {} registered {}'.format(
-                                    node['ip'], ip))
+                                print(m.format(name, node['ip'], ip))
                             break
                     else:
                         self.pr_msg(TmMsg.no_dnsmasq_conf(node['node']))
@@ -530,9 +555,6 @@ class Tm(object):
         run('tar xzpf {}.tar.gz -C {}'.format(p/'base'/self.fsversion, p/user))
         run('chown {}:{} {}'.format(user, user, p/user/self.fsversion))
         self.pr_msg(TmMsg.success('user', 'add'))
-
-    def log(self, output):
-        self.output = output
 
     def owner_or_root(self, node, needuser=True):
         r = self.db.get(Query().node == node)
